33921a66a0464ff9b5c8f06af053a4c59fab7d22
liuyx <liuyx3@foxmail.com>
2026-05-09 14:57:49 +0800

    chore: remove HomeBox (not needed)

--- diy-part1.sh
+++ diy-part1.sh
@@ -25,4 +25,3 @@ git clone --depth 1 --filter=blob:none https://github.com/vernesong/OpenClash /t
 
 git clone --depth 1 --filter=blob:none https://github.com/EasyTier/luci-app-easytier /tmp/easytier-tmp && mv /tmp/easytier-tmp/luci-app-easytier package/luci-app-easytier && mv /tmp/easytier-tmp/easytier package/easytier && rm -rf /tmp/easytier-tmp
 
-git clone --depth 1 --filter=blob:none https://github.com/jjm2473/openwrt-apps /tmp/openwrt-apps && mv /tmp/openwrt-apps/luci-app-homebox package/luci-app-homebox && mv /tmp/openwrt-apps/homebox package/homebox && rm -rf /tmp/openwrt-apps
